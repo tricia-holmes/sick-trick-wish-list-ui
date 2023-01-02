0763fed9abf837b0c7e524261fb15d6306d1488b
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-02 14:58:13 -0600

    Add fetch request and Trick component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,44 @@
-import { Component } from 'react';
-import './App.css';
+import { useCallback, useEffect, useState } from 'react'
+import './App.css'
+import API_ROUTES from '../../utilis/constants'
+import Trick from '../Trick/Trick'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Sick Trick Wish List</h1>
-      </div>
-    );
-  }
+export default function App() {
+  const [tricks, setTricks] = useState([])
+
+  const fetchData = useCallback(async () => {
+    const response = await fetch(API_ROUTES.GET_TRICKS, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+
+    const newTricks = data.map((trick) => {
+      return (
+        <Trick
+          key={`trick ${trick.id}`}
+          name={trick.name}
+          stance={trick.stance}
+          obstacle={trick.obstacle}
+          url={trick.tutorial}
+        />
+      )
+    })
+
+    setTricks(newTricks)
+    console.log(data)
+  }, [])
+
+  useEffect(() => {
+    fetchData().catch(console.error)
+  }, [])
+
+  return (
+    <div className='App'>
+      <h1>Sick Trick Wish List</h1>
+      <div className='tricks'>{tricks}</div>
+    </div>
+  )
 }
-
-export default App;
